2fefd524408375baf2b2a64082e03abd1e6d1034
Eric Chen <eric@fakeemail.com>
2024-07-03 16:29:32 +0800

    test feed function paste feed to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,74 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import PostCard from "@components/PostCard";
+
+import LoadingPostBox from "@components/LoadingPostBox";
 import Feed from "@components/Feed";
 
+const PostCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="flex flex-col gap-5">
+      {data
+        .slice()
+        .reverse()
+        .map((post) => (
+          <PostCard
+            key={[post._id]}
+            post={post}
+            handleTagClick={handleTagClick}
+            postLen={10}
+          />
+        ))}
+    </div>
+  );
+};
 const Home = () => {
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/post");
+      if (response.ok) {
+        const data = await response.json();
+
+        setPost(data);
+        console.log(data);
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="app">
-      <Feed />
+      <section className="innerApp">
+        {loading ? (
+          <div className="flex flex-col gap-4">
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+            <LoadingPostBox />
+          </div>
+        ) : (
+          <PostCardList data={post} handleTagClick={() => {}} />
+        )}
+      </section>
     </section>
   );
+  // return (
+  //   <section className="app">
+  //     {/* <Feed /> */}
+
+  //   </section>
+  // );
 };
 
 export default Home;
